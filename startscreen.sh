1d1758b5226acfa1c25ebda7e7f5b35ce6dae33d
maassander <sander.maas@student.ap.be>
2016-01-14 18:30:23 +0100

    Create startscreen.sh

---
+++ startscreen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while [ true ]
+do
+	for i in {1..3}
+	do
+		echo “$(date +%T) scan.sh cycles >> /tmp/project.log
+		bash scan.sh
+		bash killwifite.sh
+		sleep 5s
+	done
+	for i in {1..2}
+	do
+		echo “$(date +%T) scanWPA.sh cycles >> /tmp/project.log
+		bash scanWPA.sh
+		bash killwifite.sh
+		sleep 5s
+	done
+if [ -f /root/cracked.csv ]
+		then
+			echo “file cracked.csv bestaat”
+			sleep 10s
+			bash connmaker.sh
+			sleep 10s
+			bash conncheck.sh
+	fi
+done
